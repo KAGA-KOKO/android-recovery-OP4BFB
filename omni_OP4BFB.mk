9fd4202b5bc23aa604f5d45cfcd99fec668ef828
VIOLET-EVERGARDE <kotakandis185@gmail.com>
2024-06-08 21:59:30 +0700

    Update omni_OP4BFB.mk

--- omni_OP4BFB.mk
+++ omni_OP4BFB.mk
@@ -18,9 +18,11 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/pb/config/common.mk)
 
 # Device identifier.
 PRODUCT_BRAND := OPPO
