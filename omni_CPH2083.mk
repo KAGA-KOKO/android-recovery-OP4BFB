c14a35757d6cb81e713fc5fea20802587a5f39ab
KAGA-KOKO <kotakandis185@gmail.com>
2024-08-19 22:11:00 +0700

    CPH2083: change for pbrp

--- omni_CPH2083.mk
+++ omni_CPH2083.mk
@@ -17,9 +17,10 @@
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
-# Inherit from our custom product configuration
+# Specify phone tech before including full_phone
 $(call inherit-product, vendor/pb/config/common.mk)
 
 # Device identifier.
